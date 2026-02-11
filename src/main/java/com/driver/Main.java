f92673c97f86aaad537eb7ffed83b3d78c939c8c
Prem935 <prem886044@gmail.com>
2026-02-11 15:45:37 +0530

    second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,16 @@
 package com.driver;
 
 public class Main {
-	public class Product{
-		public int product(int x, int y) {};
-		public int product(int x, int y, int z) {};
-		public double product(double x, double y) {};
+	static class Product{
+		public int product(int x, int y) {
+			return x*y;
+		};
+		public int product(int x, int y, int z) {
+			return x*y*z;
+		};
+		public double product(double x, double y) {
+			return x*y;
+		};
 	}
 
 	public static void main(String[] args){
